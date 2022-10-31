c1ea8b69bd1bb4531ebdea54b6d2b5fe5ebe5f8c
Dinesh <djeeva1515@gmail.com>
2022-10-31 12:32:50 +0530

    New-Project Added

---
+++ React-Router-Proj/src/App.js
@@ -0,0 +1,29 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQoutes";
+import Layout from "./component/layout/Layout";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
