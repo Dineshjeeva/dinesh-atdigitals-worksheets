a75722151befce506a1d87de5d909f0db3b2aac3
Dinesh <djeeva1515@gmail.com>
2022-11-24 18:32:42 +0530

    AllUpdates

--- react-basics-project/src/Caluculator/CalculatorPage.tsx
+++ react-basics-project/src/Caluculator/CalculatorPage.tsx
@@ -13,13 +13,12 @@ import { ChangeEvent, useState } from "react";
 
 export default function CalculatorPage() {
   let localValue = JSON.parse(`${window.localStorage.getItem("value1") || ""}`);
-  console.log("@@@@@@@@@@" + localValue);
+  console.log("&&&&&))))(((" + localValue.result);
   const [field, setField] = useState<number>(localValue.field1);
   const [field2, setField2] = useState<number>(localValue.field2);
   const [operation, setOperation] = useState<string>(localValue.operation);
   const [equal, setEqual] = useState<string | number>(localValue.result);
-  const [save, setSave] = useState<string>();
-
+  const [save, setSave] = useState<string[]>(localValue.Values);
   const handleFieldChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -113,85 +112,100 @@ export default function CalculatorPage() {
 
     setOperation(operation);
   };
-  console.log("+++++" + equal);
+  console.log("+++++" + typeof equal);
 
   const data = {
     field1: field,
     operation: operation,
     field2: field2,
-    result: equal,
+    result: equal && equal.toString(),
+    Values: save,
   };
+
   window.localStorage.setItem("value1", JSON.stringify(data));
 
   const SaveHandler = () => {
-    const Result = field + "" + operation + "" + field2 + "=" + equal + "";
+    const Result = [field + "" + operation + "" + field2 + "=" + equal + ""];
 
-    setSave(Result);
+    if (isNaN(field) || isNaN(field2)) {
+      setSave([...save]);
+    } else {
+      setSave([...save, Result[0]]);
+    }
+    console.log(Result);
   };
+
   return (
-    <Box margin={"30px"} display={"flex"} alignItems={"center"}>
-      <Box marginRight={"30px"}>
-        <TextField
-          onKeyUp={sumHandlerChange}
-          onChange={handleFieldChange}
-          value={field}
-          label="Value 1"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-      </Box>
-      <Box marginRight={"30px"}>
-        {" "}
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 220 }}>
-          <InputLabel id="demo-simple-select-standard-label">
-            Operations
-          </InputLabel>
-          <Select
+    <Box margin={"30px"} alignItems={"center"}>
+      <Box display="flex">
+        <Box marginRight={"30px"}>
+          <TextField
             onKeyUp={sumHandlerChange}
-            value={operation}
-            onChange={handleOperation}
-            label="Operations"
+            onChange={handleFieldChange}
+            value={field}
+            label="Value 1"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="standard"
+          />
+        </Box>
+        <Box marginRight={"30px"}>
+          {" "}
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 220 }}>
+            <InputLabel id="demo-simple-select-standard-label">
+              Operations
+            </InputLabel>
+            <Select
+              onKeyUp={sumHandlerChange}
+              value={operation}
+              onChange={handleOperation}
+              label="Operations"
+            >
+              <MenuItem value={"+"}>+</MenuItem>
+              <MenuItem value={"-"}>-</MenuItem>
+              <MenuItem value={"*"}>*</MenuItem>
+              <MenuItem value={"/"}>/</MenuItem>
+              <MenuItem value={"^"}>^</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Box marginRight={"30px"}>
+          <TextField
+            onKeyUp={sumHandlerChange}
+            onChange={handleFieldChange2}
+            value={field2}
+            id="standard-number"
+            label="Value 2"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="standard"
+          />
+        </Box>
+        <Box display={"flex"} alignItems="center" marginLeft={"20px"}>
+          <Typography> = {equal}</Typography>
+        </Box>
+
+        <Box display={"flex"} alignItems="center" marginLeft={"20px"}>
+          <Button
+            style={{ display: "flex", padding: "10px" }}
+            onClick={SaveHandler}
+            variant="contained"
           >
-            <MenuItem value={"+"}>+</MenuItem>
-            <MenuItem value={"-"}>-</MenuItem>
-            <MenuItem value={"*"}>*</MenuItem>
-            <MenuItem value={"/"}>/</MenuItem>
-            <MenuItem value={"^"}>^</MenuItem>
-          </Select>
-        </FormControl>
+            Save
+          </Button>
+        </Box>
       </Box>
-
-      <Box marginRight={"30px"}>
-        <TextField
-          onKeyUp={sumHandlerChange}
-          onChange={handleFieldChange2}
-          value={field2}
-          id="standard-number"
-          label="Value 2"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-      </Box>
-      <Box display={"flex"} alignItems="center" marginLeft={"20px"}>
-        <Typography> = {equal}</Typography>
-      </Box>
-
-      <Box display={"flex"} alignItems="center" marginLeft={"20px"}>
-        <Button
-          style={{ display: "flex", padding: "10px" }}
-          onClick={SaveHandler}
-          // style={{ left: "30px" }}
-          variant="contained"
-        >
-          Save
-        </Button>
-        <Typography marginLeft={"10px"}>{save}</Typography>
+      <Box>
+        {save.map((item, index) => (
+          <Typography key={index} marginLeft={"10px"}>
+            StorageValues: {item}{" "}
+          </Typography>
+        ))}
       </Box>
     </Box>
   );
